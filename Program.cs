cc4f1b53836bb1840c0b3c540be64bf982c68ab6
Madina <madina.orish@mail.ru>
2022-08-28 13:23:24 +0300

    1 zadacha

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+int num;
+int secondNumber() {
+
+
+int dec = (num % 100)/10;
+return dec;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+int dec = secondNumber();
+Console.WriteLine("Вторая цифра введенного числа: " + dec);
