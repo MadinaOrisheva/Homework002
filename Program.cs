b3bde802431597a9f9924d92c711660246c3496f
Madina <madina.orish@mail.ru>
2022-08-28 17:51:04 +0300

    добавила бесконечный цикл

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@
 int num;
 int secondNumber() {
 
-
 int dec = (num % 100)/10;
 return dec;
 }
-
+// испольховала бесконечный цикл для удобства проверки выполнения условия с разными числами
+while (true) {
 Console.WriteLine("Введите трехзначное число: ");
 num = Convert.ToInt32(Console.ReadLine());
 int dec = secondNumber();
 Console.WriteLine("Вторая цифра введенного числа: " + dec);
+}
