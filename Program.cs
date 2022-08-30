d2448c5da7f845afca84995f88580de17483ec57
Madina <madina.orish@mail.ru>
2022-08-30 18:05:24 +0300

    3 zadacha ispravlena

--- Program.cs
+++ Program.cs
@@ -66,4 +66,9 @@ void Day (int num) {
 
 Console.WriteLine("Введите число от 1 до 7 :");
 int dayNumber=Convert.ToInt32(Console.ReadLine());
-Day(dayNumber);
+if (dayNumber>7 || dayNumber<1)
+{
+  Console.WriteLine("Вы ввели недопустимое число");
+  }
+else {
+  Day(dayNumber);}
